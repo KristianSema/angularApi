38e4a614472fe33ed147e9366fa433ba97afa0de
KristianSema <sema.kristian@iisgalvanimi.edu.it>
2021-01-05 11:20:20 +0000

    punto 6 e 7

--- semaverifica/src/app/item/item.component.ts
+++ semaverifica/src/app/item/item.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import {Location} from '@angular/common'
+import { GamesService } from '../games.service';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+  routeObs: Observable<ParamMap>;
+
+  games : any;selezionata
+  gameServiceObs: Observable<Object>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private service: GamesService,
+    private location: Location ) { }
 
   ngOnInit(): void {
+    this.routeObs = this.route.paramMap;
+    this.routeObs.subscribe(this.getRouterParam);
   }
-
+  getRouterParam = (params: ParamMap) =>
+  {
+    let gameId = params.get('id');
+    console.log (gameId);
+    this.gameServiceObs = this.service.gamesDetails(gameId) ;
+    this.gameServiceObs.subscribe((data)=>this.games = data)
+  }
 }
